31df45eab3ac7e33aff47184bb029275cc3a411c
santos-naudiyal <84222082+santos-naudiyal@users.noreply.github.com>
2024-05-17 22:16:30 +0530

    clean the un-used code

--- lib/views/categorypage.dart
+++ lib/views/categorypage.dart
@@ -103,48 +103,7 @@ Future<void> _refreshNews() async {
 
 
 
-/*
-class NewsTemplate extends StatelessWidget {
-  String title, description, url, urlToImage;
-  NewsTemplate(
-      {required this.title,
-      required this.description,
-      required this.url,
-      required this.urlToImage});
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(16),
-      child: Column(
-        children: <Widget>[
-          ClipRRect(
-              borderRadius: BorderRadius.circular(6),
-              child: CachedNetworkImage(
-                imageUrl: urlToImage,
-                width: 380,
-                height: 200,
-                fit: BoxFit.cover,
-              )),
-          SizedBox(height: 8),
-          Text(
-            title,
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 18.0,
-                color: Colors.black),
-          ),
-          SizedBox(height: 8),
-          Text(
-            description,
-            style: TextStyle(fontSize: 15.0, color: Colors.grey[800]),
-          ),
-        ],
-      ),
-    );
-  }
-}
-*/
 
 class NewsTemplate extends StatelessWidget {
   final String? urlToImage;
